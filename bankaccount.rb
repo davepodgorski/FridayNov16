f89d8e3edeb7d30696dfd445286b7d378147a38a
davepodgorski <davepodgorski@gmail.com>
2018-11-16 14:21:46 -0500

    bankaccount

---
+++ bankaccount.rb
@@ -0,0 +1,36 @@
+# https://www.youtube.com/watch?v=dAH4zGd_W1s
+class BankAccount
+  def initialize (balance, interest_rate)
+    @balance = balance
+    @interest_rate = interest_rate
+  end
+
+  def balance
+    return @balance
+  end
+
+  def interest_rate
+    return @interest_rate
+  end
+
+  def deposit(x)
+      @balance = balance + x
+      return @balance
+  end
+
+  def withdraw(y)
+    @balance = balance - y
+    return @balance
+  end
+
+  def gain_interest
+    @balance = balance * interest_rate.to_f
+    return @balance
+    return
+  end
+end
+  Davesbankaccount = BankAccount.new(50, 1.02)
+  Davesbankaccount.deposit(50)
+  Davesbankaccount.withdraw(1)
+  Davesbankaccount.gain_interest
+  puts Davesbankaccount.balance
